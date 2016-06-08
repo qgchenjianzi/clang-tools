d70940026070252b9dbc6edfd1a5777c7e7a53e9
陈坚梓 <currychen@currychen-MC1.local>
2016-06-08 19:34:30 +0800

    fix the input file

--- src/clangToolsMain.cpp
+++ src/clangToolsMain.cpp
@@ -1,18 +1,24 @@
 /**
- * File : clangToolsMain.cpp
- * author: currychen
- * email: qgchenjianzi@foxmail.com
- */
+* File : clangToolsMain.cpp
+* author: currychen
+* email: qgchenjianzi@foxmail.com
+*/
 #include <iostream>
+#include <fstream>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <dirent.h>
 
+
 using namespace std;
 
 string file_format_m = ".m";
 string file_format_mm = ".mm";
+
+string file_prj_dir = ".prjDir";
+string file_tools_dir = ".toolsDir";
 
 string dir_main_src ;
 string dir_tool = "build/";
@@ -23,7 +29,7 @@ int isCodeFile(string fileName)
 {
     size_t iPos = fileName.find_last_of(".");
     if(iPos <= 0)
-        return 0;
+    return 0;
     string fileType = fileName.substr(iPos,fileName.length()-1);
     if(strcmp(fileType.c_str(),file_format_m.c_str())==0 || strcmp(fileType.c_str(),file_format_mm.c_str())==0)
     {
@@ -59,38 +65,25 @@ int traversePrj(const char *prjDir,const char *toolsDir)
     while((file = readdir(dPrj))!=NULL)
     {
         if(strcmp(file->d_name,".")==0 || strcmp(file->d_name,"..")==0 )
-            continue;
-
-
+        continue;
         //else if (stat(file->d_name,&sb)==0 && S_ISREG(sb.st_mode) && isCodeFile(file->d_name)==1 ) 
-        /*if(stat(file->d_name,&sb)>=0 && S_ISDIR(sb.st_mode)) 
-          {
-          cout << "file name: " << file->d_name << endl;
-          traversePrj(file->d_name,toolsDir);
-          }
-          else if(isCodeFile(file->d_name))
-          {
+        if(stat(file->d_name,&sb)>=0 && S_ISDIR(sb.st_mode)) 
+        {
+            cout << "scan folder name: " << file->d_name << endl;
+            traversePrj(file->d_name,toolsDir);
+        }
+        else if(isCodeFile(file->d_name))
+        {
+            string strToolsDir = toolsDir + tool_name;
+            string handleFile = strPrjDir+"/"+file->d_name;
+            string callTools = strToolsDir + " " + handleFile;
 
-          string strToolsDir = toolsDir;
-          string handleFile = file->d_name;
-          string callTools = strToolsDir + " " + handleFile;
-
-          cout << "file name: " << file->d_name << endl;
-          cout << "callTools sys" << endl;
-          system(callTools.c_str());  
-          }*/
-        if(isCodeFile(file->d_name)){
-        string strToolsDir = toolsDir + tool_name;
-        string handleFile = strPrjDir+"/"+file->d_name;
-        string callTools = strToolsDir + " " + handleFile;
-
-        cout << "the cmd is " << callTools << endl;
-        cout << "file name: " << file->d_name << endl;
-        system(callTools.c_str());  
+            cout << "the cmd is " << callTools << endl;
+            cout << "file name: " << file->d_name << endl;
+            system(callTools.c_str());  
         }
     }
-    if(dPrj!=NULL)
-        closedir(dPrj);
+    closedir(dPrj);
     return 0;
 }
 void initStr()
@@ -98,12 +91,38 @@ void initStr()
 
 }
 
+/**
+* 检查文件是否可以打开
+*/
+int isFolderAccess(char *dir)
+{
+    if(access(dir,F_OK)==0)
+    return 0;
+    return -1;
+}
+
+/**
+* 以命令行的形式读入项目路径和工具路径
+*/
 int getDir(char *dir,int size)
 {
     cin.getline(dir,size);  
-    if(access(dir,F_OK) == 0)
-        return 0;
-    return -1;
+    return isFolderAccess(dir);
+}
+
+/**
+* 以文件配置的形式读入项目路径和工具路径
+*/
+int getDir(char *dir,int size,string fileName)
+{
+    ifstream readFile(fileName);
+    if(!readFile.is_open())
+    {
+        cout << "Error opening file!" << endl;
+    }
+    while(!readFile.eof())
+        readFile.getline(dir,size); 
+    return isFolderAccess(dir);
 }
 
 int main(int argc , char *argv[])
@@ -111,39 +130,41 @@ int main(int argc , char *argv[])
     char prjDir[100];
     char toolsDir[100];
 
-    cout << "Please input the project dir:" << endl; 
-    while(getDir(prjDir,sizeof(prjDir))!=0)
+    cout << "The project dir is :" << endl; 
+    if(getDir(prjDir,sizeof(prjDir),file_prj_dir)!=0)
     {
         system("clear");
         cout << prjDir << " The project dir cannot open!" << endl;
         cout << "Please input the rigit project dir!" << endl;
+        return 0;
     }
 
-    cout << "Please input the clang tools dir:" << endl; 
-    while(getDir(toolsDir,sizeof(toolsDir))!=0)
+    cout << "The tools dir is :" << endl; 
+    if(getDir(toolsDir,sizeof(toolsDir),file_tools_dir)!=0)
     {
         system("clear");
         cout << toolsDir << " The clang tools dir cannot open!" << endl;
         cout << "Please input the rigit clang tools dir!" << endl;
+        return 0;
     }
+
+
 
     string strPrjDir(prjDir);
     size_t iPos = strPrjDir.rfind('/');
     string prjName = strPrjDir.substr(iPos+1,strPrjDir.length()-1);
 
     if(strPrjDir.back() == '/')
-        dir_main_src = strPrjDir + prjName;
+    dir_main_src = strPrjDir + prjName;
     else
-        dir_main_src = strPrjDir + "/" + prjName;
+    dir_main_src = strPrjDir + "/" + prjName;
 
 
     string strToolDir(toolsDir);
     if(strToolDir.back() == '/')
-        dir_tool = strToolDir + dir_tool;
+    dir_tool = strToolDir + dir_tool;
     else 
-        dir_tool = strToolDir + "/" + dir_tool;
+    dir_tool = strToolDir + "/" + dir_tool;
 
     traversePrj(dir_main_src.c_str(),dir_tool.c_str());
-
-
 }
